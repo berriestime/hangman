53145184fd6d7723e7d5b9aa0f776b74e44db233
Elena Koroleva <berries.whale@gmail.com>
2024-01-16 12:08:42 +0300

    refactor: change innerHTML to createElement

--- hangman/src/index.js
+++ hangman/src/index.js
@@ -5,22 +5,11 @@ import humanArmLeft from './../assets/human-arm-left.svg';
 import humanArmRight from './../assets/human-arm-right.svg';
 import humanLegLeft from './../assets/human-leg-left.svg';
 import humanLegRight from './../assets/human-leg-right.svg';
+import gallows from './../assets/gallows.svg';
 import jsonData from './questions.json';
 
 const mainContainer = document.createElement('div');
 mainContainer.classList.add('main-container');
-
-const gallowsContainer = document.createElement('div');
-gallowsContainer.classList.add('gallows-container');
-gallowsContainer.innerHTML = `
-      <svg id="gallows-svg" width="500" height="581" viewBox="0 0 500 581" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <rect x="211.337" y="34.6662" width="39" height="199.598" transform="rotate(45 211.337 34.6662)" fill="black" stroke="#FFFEFE" stroke-width="3"/>
-        <rect x="69.5" y="1.5" width="39" height="578" rx="3.5" fill="black" stroke="#FFFEFE" stroke-width="3"/>
-        <rect x="386.5" y="34.5" width="39" height="350" rx="3.5" transform="rotate(90 386.5 34.5)" fill="black" stroke="#FFFEFE" stroke-width="3"/>
-        <rect x="498.5" y="540.5" width="39" height="497" rx="3.5" transform="rotate(90 498.5 540.5)" fill="black" stroke="#FFFEFE" stroke-width="3"/>
-        <rect x="333" y="75" width="10" height="74" fill="black"/>
-      </svg>
-    `;
 
 const title = document.createElement('h1');
 title.classList.add('title');
@@ -28,23 +17,35 @@ title.textContent = 'HANGMAN GAME';
 
 const mainContainerPart1 = document.createElement('div');
 mainContainerPart1.classList.add('main-container__part');
-mainContainerPart1.appendChild(gallowsContainer);
-mainContainerPart1.appendChild(title);
+
+const gallowsContainer = document.createElement('img');
+gallowsContainer.classList.add('gallows-container');
+gallowsContainer.src = gallows;
+mainContainerPart1.append(gallowsContainer);
+
+mainContainerPart1.append(gallowsContainer);
+mainContainerPart1.append(title);
 
 const game = document.createElement('div');
 game.id = 'game';
 
 const wordToGuess = document.createElement('p');
 wordToGuess.id = 'wordToGuess';
-wordToGuess.innerHTML = '<span id="word"></span>';
+const wordSpan = document.createElement('span');
+wordSpan.id = 'word';
+wordToGuess.append(wordSpan);
 
 const hint = document.createElement('p');
 hint.id = 'wordToGuess';
-hint.innerHTML = '<span id="hint"></span>';
+const hintSpan = document.createElement('span');
+hintSpan.id = 'hint';
+hint.append(hintSpan);
 
 const guesses = document.createElement('p');
 guesses.id = 'guesses';
-guesses.innerHTML = '<span id="wrongGuesses"></span>';
+const guessSpan = document.createElement('span');
+guessSpan.id = 'wrongGuesses';
+guesses.append(guessSpan);
 
 const statusDisplay = document.createElement('p');
 statusDisplay.id = 'status';
@@ -52,52 +53,46 @@ statusDisplay.id = 'status';
 const keyboard = document.createElement('div');
 keyboard.id = 'keyboard';
 
-game.appendChild(wordToGuess);
-game.appendChild(hint);
-game.appendChild(guesses);
-game.appendChild(statusDisplay);
-game.appendChild(keyboard);
+game.append(wordToGuess);
+game.append(hint);
+game.append(guesses);
+game.append(statusDisplay);
+game.append(keyboard);
 
 const mainContainerPart2 = document.createElement('div');
 mainContainerPart2.classList.add('main-container__part');
-mainContainerPart2.appendChild(game);
+mainContainerPart2.append(game);
 
-mainContainer.appendChild(mainContainerPart1);
-mainContainer.appendChild(mainContainerPart2);
+mainContainer.append(mainContainerPart1);
+mainContainer.append(mainContainerPart2);
 
-document.body.appendChild(mainContainer);
-
-const gallows = document.querySelector('.gallows-container');
+document.body.append(mainContainer);
 
 function displayPartOfBody(partsDrawn) {
   const bodyPart = document.createElement('img');
   bodyPart.classList.add('bodypart');
   if (partsDrawn === 1) {
     bodyPart.src = humanHead;
-    gallows.appendChild(bodyPart);
+    mainContainerPart1.append(bodyPart);
   } else if (partsDrawn === 2) {
     bodyPart.src = humanBody;
-    gallows.appendChild(bodyPart);
+    mainContainerPart1.append(bodyPart);
   } else if (partsDrawn === 3) {
     bodyPart.src = humanArmLeft;
-    gallows.appendChild(bodyPart);
+    mainContainerPart1.append(bodyPart);
   } else if (partsDrawn === 4) {
     bodyPart.src = humanArmRight;
-    gallows.appendChild(bodyPart);
+    mainContainerPart1.append(bodyPart);
   } else if (partsDrawn === 5) {
     bodyPart.src = humanLegLeft;
-    gallows.appendChild(bodyPart);
+    mainContainerPart1.append(bodyPart);
   } else if (partsDrawn === 6) {
     bodyPart.src = humanLegRight;
-    gallows.appendChild(bodyPart);
+    mainContainerPart1.append(bodyPart);
   }
 }
 
 const maxWrongGuesses = 6;
-
-const wordDisplay = document.querySelector('#word');
-const hintDisplay = document.querySelector('#hint');
-const wrongGuessesDisplay = document.querySelector('#wrongGuesses');
 
 let word = pickRandom(jsonData);
 let rightGuesses = [];
@@ -119,13 +114,18 @@ function resetGame() {
   updateDisplay();
 }
 function updateDisplay() {
-  wordDisplay.innerHTML = word.word
+  wordSpan.textContent = word.word
     .split('')
     .map((letter) => (rightGuesses.includes(letter) ? letter : '__'))
     .join(' ');
 
-  hintDisplay.innerHTML = `Hint: ${word.hint}`;
-  wrongGuessesDisplay.innerHTML = `Incorrect guesses: <span class="wrongGuessesCount">${wrongGuessesCount}/6</span>`;
+  hintSpan.textContent = `Hint: ${word.hint}`;
+  guessSpan.textContent = `Incorrect guesses: `;
+
+  const wrongGuessesSpan = document.createElement('span');
+  wrongGuessesSpan.className = 'wrongGuessesCount';
+  wrongGuessesSpan.textContent = `${wrongGuessesCount}/6`;
+  guessSpan.append(wrongGuessesSpan);
 }
 
 function clearKeyboard() {
@@ -141,7 +141,7 @@ function hideBodyParts() {
   bodyparts.forEach((x) => x.remove());
 }
 function clearStatus() {
-  statusDisplay.innerHTML = '';
+  statusDisplay.textContent = '';
 }
 
 createKeyboard();
@@ -164,13 +164,13 @@ function createKeyboard() {
   for (let i = 65; i <= 90; i++) {
     const button = document.createElement('button');
     const letter = String.fromCharCode(i);
-    button.innerHTML = letter;
+    button.textContent = letter;
     button.classList.add('key');
     button.setAttribute('data-key', letter.toLowerCase());
     button.addEventListener('click', function () {
       handleKeyboardInput(letter);
     });
-    keyboard.appendChild(button);
+    keyboard.append(button);
   }
 
   document.addEventListener('keydown', function (event) {
@@ -206,22 +206,10 @@ function disableKeyboard() {
 function showModal(message) {
   document.body.classList.add('noscroll');
   const modalBackdrop = document.createElement('div');
-  modalBackdrop.style.position = 'fixed';
-  modalBackdrop.style.left = '0';
-  modalBackdrop.style.top = '0';
-  modalBackdrop.style.width = '100%';
-  modalBackdrop.style.height = '100%';
-  modalBackdrop.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-  modalBackdrop.style.display = 'flex';
-  modalBackdrop.style.justifyContent = 'center';
-  modalBackdrop.style.alignItems = 'center';
-  modalBackdrop.style.zIndex = '1000';
+  modalBackdrop.classList.add('backdrop');
 
   const modalContent = document.createElement('div');
-  modalContent.style.background = 'white';
-  modalContent.style.padding = '20px';
-  modalContent.style.borderRadius = '5px';
-  modalContent.style.textAlign = 'center';
+  modalContent.classList.add('modal-content');
 
   const modalText = document.createElement('p');
   modalText.textContent = message;
@@ -232,13 +220,14 @@ function showModal(message) {
   closeButton.textContent = 'Start new game';
   closeButton.onclick = function () {
     document.body.removeChild(modalBackdrop);
+    document.body.classList.remove('noscroll');
     resetGame();
     console.log(word.word);
   };
-  modalContent.appendChild(modalText);
-  modalContent.appendChild(closeButton);
-  modalBackdrop.appendChild(modalContent);
-  document.body.appendChild(modalBackdrop);
+  modalContent.append(modalText);
+  modalContent.append(closeButton);
+  modalBackdrop.append(modalContent);
+  document.body.append(modalBackdrop);
 }
 
 function checkGameStatus() {
